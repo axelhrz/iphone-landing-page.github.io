ccc189d4ed1e69f29ce6b9e4dd8afe958ee3074f
axelhrz <akselhrz@gmail.com>
2022-06-03 13:29:15 +0100

    first upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let img = document.getElementById("main_img");
+let container = document.querySelector(".main_sec");
+
+
+function phones(src) {
+    img.src = src;
+}
+
+function colors(color) {
+    container.style.background = color;
+
+}
